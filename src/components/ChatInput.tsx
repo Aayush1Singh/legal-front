62ab8832a12e34349354d089420a6b13acf01cd4
AAYUSH SINGH <aayush3708@outlook.com>
2025-06-22 19:24:56 +0530

    updating input

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -121,7 +121,17 @@ const ChatInput: React.FC<ChatInputProps> = ({
             type="file"
             accept=".pdf"
             onChange={handleFileUpload}
-            className="hidden"
+            style={{
+              position: "absolute",
+              width: 1,
+              height: 1,
+              padding: 0,
+              margin: 0,
+              overflow: "hidden",
+              clip: "rect(0 0 0 0)",
+              whiteSpace: "nowrap",
+              border: 0,
+            }}
           />
 
           {/* Attachment Button */}
